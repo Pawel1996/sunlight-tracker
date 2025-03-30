7c3267e887a8bc683b6b5b54f7b9188dc2b018c2
pawel <pawel1996k@gmail.com>
2025-03-31 00:08:09 +0200

    second commit

---
+++ src/main/java/com/example/sunlight_tracker/SunlightTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sunlight_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlightTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunlightTrackerApplication.class, args);
+	}
+
+}
